08b4f35e80ede550622e8027e46e8d6effca5bd5
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2025-09-13 11:07:35 -0400

    CI: start save-and-restore service

---
+++ container/create_env_file.py
@@ -0,0 +1,10 @@
+import socket
+
+if __name__ == "__main__":
+    # Get the local IP address
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+
+    with open(".env", "w") as f:
+        f.write(f"HOST_EXTERNAL_IP_ADDRESS={ip}")
