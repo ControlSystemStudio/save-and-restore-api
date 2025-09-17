b2b4091a075b8ee2f59d1d93e507c19e3193a19c
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2025-09-17 11:30:46 -0400

    ENH: refactor of the 'upload' CLI

--- src/save_and_restore_api/tools/upload.py
+++ src/save_and_restore_api/tools/upload.py
@@ -1,107 +1,19 @@
-import getpass
-import logging
-import pprint
+import argparse
 
-import httpx
+# import getpass
+import logging
+import os
+
+# import pprint
+import save_and_restore_api
+
+version = save_and_restore_api.__version__
 
 logger = logging.getLogger(__name__)
 
 BASE_URL = "http://epics-services-hxn.nsls2.bnl.local:20381/save-restore"
 timeout = 2
 file_name = "auto_settings.sav"
-
-
-class SaveRestoreAPI:
-    def __init__(self, *, base_url, timeout):
-        self._base_url = base_url
-        self._timeout = timeout
-        self._client = None
-        self._root_node_uid = "44bef5de-e8e6-4014-af37-b8f6c8a939a2"
-
-        self._username = None
-        self._password = None
-        # self._username = "***REMOVED***"
-        # self._password = "***REMOVED***"
-
-    @property
-    def ROOT_NODE_UID(self):
-        return self._root_node_uid
-
-    def open(self):
-        auth = httpx.BasicAuth(username=self._username, password=self._password)
-        self._client = httpx.Client(base_url=self._base_url, timeout=timeout, auth=auth)
-
-    def close(self):
-        self._client.close()
-        self._client = None
-
-    def set_username_password(self, username=None, password=None):
-        if not isinstance(username, str):
-            print("Username: ", end="")
-            username = input()
-        if not isinstance(password, str):
-            password = getpass.getpass()
-
-        self._username = username
-        self._password = password
-
-    def login(self, *, username=None, password=None):
-        params = {"username": self._username, "password": self._password}
-        self.send_request("POST", "/login", json=params)
-
-    def send_request(self, method, url, **kwargs):
-        response = self._client.request(method, url, **kwargs)
-
-        print(f"{response.request.url=}")
-        print(f"{response.headers.get('content-type')=}")
-
-        if response.status_code != 200:
-            print(f"Request failed: status code {response.status_code}")
-            print(f"Error message: {response.text}")
-            raise Exception(f"Request failed with code {response.status_code}")
-
-        if response.headers.get("content-type") == "application/json":
-            data = response.json()
-        else:
-            data = {}
-
-        return data
-
-    def get_node(self, node_uid):
-        return self.send_request("GET", f"/node/{node_uid}")
-
-    def get_children(self, node_uid):
-        return self.send_request("GET", f"/node/{node_uid}/children")
-
-    def create_config(self, parent_node_uid, name, pv_list):
-        config_dict = {
-            "configurationNode": {
-                "name": name,
-                "nodeType": "CONFIGURATION",
-                "userName": self._username,
-            },
-            "configurationData": {
-                "pvList": pv_list,
-            },
-        }
-        print(f"config_dict=\n{pprint.pformat(config_dict)}")
-        return self.send_request("PUT", f"/config?parentNodeId={parent_node_uid}", json=config_dict)
-
-    def update_config(self, node_uid, name, pv_list):
-        config_dict = {
-            "configurationNode": {
-                "name": name,
-                "nodeType": "CONFIGURATION",
-                "userName": self._username,
-                "uniqueId": node_uid,
-            },
-            "configurationData": {
-                "pvList": pv_list,
-            },
-        }
-        print(f"config_dict=\n{pprint.pformat(config_dict)}")
-        # return self.send_request("POST", f"/config/{node_uid}", json=config_dict)
-        return self.send_request("POST", "/config", json=config_dict)
 
 
 def add_to_pv_list(pv_list, *, pv_name):
@@ -121,34 +33,114 @@ def load_pvs_from_autosave_file(file_name):
     return pv_names
 
 
+def split_config_name(config_name):
+    if not config_name.startswith("/"):
+        config_name = "/" + config_name
+    _ = config_name.split("/")
+    folders, name = _[1:-1], _[-1]
+    return folders, name
+
+
 def main():
     logging.basicConfig(level=logging.WARNING)
-    # logging.getLogger("bluesky_queueserver").setLevel("INFO")
+    logging.getLogger("save-and-restore-api").setLevel("INFO")
 
-    SR = SaveRestoreAPI(base_url=BASE_URL, timeout=timeout)
+    def formatter(prog):
+        # Set maximum width such that printed help mostly fits in the RTD theme code block (documentation).
+        return argparse.RawDescriptionHelpFormatter(prog, max_help_position=20, width=90)
+
+    parser = argparse.ArgumentParser(
+        description="save-and-restore-upload: create configuration based on a batch of PVs.\n"
+        f"save-and-restore-api version {version}\n\n"
+        "Read a batch of PVs from a file and creates a configuration in Save and Restore service.\n",
+        formatter_class=formatter,
+    )
+
+    parser.add_argument(
+        "--file-name",
+        "-f",
+        dest="file_name",
+        type=str,
+        default=None,
+        help="File name with PV names.",
+    )
+
+    parser.add_argument(
+        "--config-name",
+        "-c",
+        dest="config_name",
+        type=str,
+        default=None,
+        help="Configuration name including folders, e.g. /detectors/imaging/eiger_config",
+    )
+
+    parser.add_argument(
+        "--create-folders",
+        dest="create_folders",
+        action="store_true",
+        help="Configuration name including folders, e.g. /detectors/imaging/eiger_config",
+    )
+
+    parser.add_argument(
+        "--update",
+        dest="config_update",
+        action="store_true",
+        help="Configuration name including folders, e.g. /detectors/imaging/eiger_config",
+    )
+
+    args = parser.parse_args()
+    file_name = args.file_name
+    config_name = args.config_name
+    create_folders = args.create_folders
+    config_update = args.config_update
+
     try:
-        pv_names = load_pvs_from_autosave_file(file_name)
+        if args.file_name is None:
+            raise ValueError("Required '--file-name' ('-f') parameter is not specified")
 
-        SR.set_username_password()
-        SR.open()
-        SR.login()
+        if args.config_name is None:
+            raise ValueError("Required '--config-name' ('-c') parameter is not specified")
 
-        data = SR.get_node(SR.ROOT_NODE_UID)
-        print(f"data=\n{pprint.pformat(data)}")
-        data = SR.get_children(data["uniqueId"])
-        print(f"data=\n{pprint.pformat(data)}")
-        parent_node_uid = data[0]["uniqueId"]
-        name = "test5"
-        pv_list = []
-        for pv_name in pv_names:
-            add_to_pv_list(pv_list, pv_name=pv_name)
-        add_to_pv_list(pv_list, pv_name="13SIM1:{SimDetector-Cam:1}cam1:BinX")
-        add_to_pv_list(pv_list, pv_name="13SIM1:{SimDetector-Cam:1}cam1:BinY")
-        data = SR.create_config(parent_node_uid, name, pv_list)
-        print(f"data=\n{pprint.pformat(data)}")
-        node_uid = data["configurationNode"]["uniqueId"]
-        data = SR.update_config(node_uid, name + "a", pv_list)
-        print(f"data=\n{pprint.pformat(data)}")
+        file_name = os.path.abspath(os.path.expanduser(file_name))
 
-    finally:
-        SR.close()
+        print(f"file_name={file_name}")
+        print(f"config_name={config_name}")
+        print(f"create_folders={create_folders}")
+        print(f"update={config_update}")
+
+        if not os.path.isfile(file_name):
+            raise ValueError(f"Input file '{file_name}' does not exist")
+
+        folders, name = split_config_name(config_name)
+        print(f"folders={folders}, name={name}")
+
+    except Exception as ex:
+        logger.error(f"Failed: {ex}")
+
+    # SR = SaveRestoreAPI(base_url=BASE_URL, timeout=timeout)
+    # try:
+    #     pv_names = load_pvs_from_autosave_file(file_name)
+
+    #     SR.set_username_password()
+    #     SR.open()
+    #     SR.login()
+
+    #     data = SR.get_node(SR.ROOT_NODE_UID)
+    #     print(f"data=\n{pprint.pformat(data)}")
+    #     data = SR.get_children(data["uniqueId"])
+    #     print(f"data=\n{pprint.pformat(data)}")
+    #     parent_node_uid = data[0]["uniqueId"]
+    #     name = "test5"
+    #     pv_list = []
+    #     for pv_name in pv_names:
+    #         add_to_pv_list(pv_list, pv_name=pv_name)
+    #     add_to_pv_list(pv_list, pv_name="13SIM1:{SimDetector-Cam:1}cam1:BinX")
+    #     add_to_pv_list(pv_list, pv_name="13SIM1:{SimDetector-Cam:1}cam1:BinY")
+    #     data = SR.create_config(parent_node_uid, name, pv_list)
+    #     print(f"data=\n{pprint.pformat(data)}")
+    #     node_uid = data["configurationNode"]["uniqueId"]
+    #     data = SR.update_config(node_uid, name + "a", pv_list)
+    #     print(f"data=\n{pprint.pformat(data)}")
+
+    # finally:
+    #     SR.close()
