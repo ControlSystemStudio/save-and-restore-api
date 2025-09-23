91792c7f32267325dd3799f40c7f00425bef8ab8
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2025-09-23 10:55:58 -0400

    ENH: add imports for treaded and async versions

--- src/save_and_restore_api/__init__.py
+++ src/save_and_restore_api/__init__.py
@@ -1,12 +1,6 @@
-"""
-Copyright (c) 2025 My Name. All rights reserved.
-
-save-and-restore-api: Python package for communication with CS Studio save-and-restore service
-"""
-
 from __future__ import annotations
 
-from ._api import SaveRestoreAPI
+from ._api_threads import _SaveRestoreAPI_Threads as SaveRestoreAPI
 from ._version import version as __version__
 
 __all__ = ["__version__", "SaveRestoreAPI"]
---
+++ src/save_and_restore_api/_api_async.py
@@ -0,0 +1,4 @@
+from ._api_base import _SaveRestoreAPI_Base
+
+
+class _SaveRestoreAPI_Async(_SaveRestoreAPI_Base): ...
