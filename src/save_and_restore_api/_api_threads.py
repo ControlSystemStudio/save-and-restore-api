fd2e3a014f7141e3639a517cc48d6d8070bfeef4
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2025-09-23 12:03:35 -0400

    ENH: 'open' and 'close' functions for threaded and async classes

--- src/save_and_restore_api/_api_threads.py
+++ src/save_and_restore_api/_api_threads.py
@@ -1,4 +1,12 @@
+import httpx
+
 from ._api_base import _SaveRestoreAPI_Base
 
 
-class _SaveRestoreAPI_Threads(_SaveRestoreAPI_Base): ...
+class _SaveRestoreAPI_Threads(_SaveRestoreAPI_Base):
+    def open(self):
+        self._client = httpx.Client(base_url=self._base_url, timeout=self._timeout)
+
+    def close(self):
+        self._client.close()
+        self._client = None
