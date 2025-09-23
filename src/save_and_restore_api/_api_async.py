5a39dd891075f37211f2128c4079516394946bcf
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2025-09-23 12:37:25 -0400

    ENH: implementation of 'send_request'

--- src/save_and_restore_api/_api_async.py
+++ src/save_and_restore_api/_api_async.py
@@ -10,3 +10,24 @@ class _SaveRestoreAPI_Async(_SaveRestoreAPI_Base):
     async def close(self):
         await self._client.aclose()
         self._client = None
+
+    async def send_request(
+        self, method, url, *, params=None, url_params=None, headers=None, data=None, timeout=None, auth=None
+    ):
+        try:
+            client_response = None
+            kwargs = self._prepare_request(
+                method=method,
+                params=params,
+                url_params=url_params,
+                headers=headers,
+                data=data,
+                timeout=timeout,
+                auth=auth,
+            )
+            client_response = await self._client.request(method, url, **kwargs)
+            response = self._process_response(client_response=client_response)
+        except Exception:
+            response = self._process_comm_exception(method=method, params=params, client_response=client_response)
+
+        return response
--- src/save_and_restore_api/_api_threads.py
+++ src/save_and_restore_api/_api_threads.py
@@ -10,3 +10,24 @@ class _SaveRestoreAPI_Threads(_SaveRestoreAPI_Base):
     def close(self):
         self._client.close()
         self._client = None
+
+    def send_request(
+        self, method, url, *, params=None, url_params=None, headers=None, data=None, timeout=None, auth=None
+    ):
+        try:
+            client_response = None
+            kwargs = self._prepare_request(
+                method=method,
+                params=params,
+                url_params=url_params,
+                headers=headers,
+                data=data,
+                timeout=timeout,
+                auth=auth,
+            )
+            client_response = self._client.request(method, url, **kwargs)
+            response = self._process_response(client_response=client_response)
+        except Exception:
+            response = self._process_comm_exception(method=method, params=params, client_response=client_response)
+
+        return response
